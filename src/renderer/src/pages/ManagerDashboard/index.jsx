2e66e75d60e656ab867b669a1bed37d406d2a309
Yi Cao <caoyi@mgtv.com>
2023-12-21 11:29:34 +0800

    feat: manager页面 layout布局

--- src/renderer/src/pages/ManagerDashboard/index.jsx
+++ src/renderer/src/pages/ManagerDashboard/index.jsx
@@ -1,13 +1,66 @@
 import { Link, Outlet } from "react-router-dom"
+import React from 'react'
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+const { Header, Content, Sider } = Layout;
 
-const Settings = () => {
+import styles from './index.module.less'
+
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  },
+);
+
+const items = [{
+  key: ''
+}]
+
+const ManagerDashboard = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <div>
-      <Link to='/'>to home</Link>
-      settings page
-      <Outlet />
-    </div>
-  )
-}
+    <Layout className={styles.layout_wrapper}>
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <Link to='/'>to home</Link>
+      </Header>
+      <Content>
+        <Layout
+          className={styles.container}
+          style={{ background: colorBgContainer }}
+        >
+          <Sider style={{ background: colorBgContainer }} width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%' }}
+              items={items2}
+            />
+          </Sider>
+          <Content className={styles.content_wrapper}>
+            <Outlet/>
+          </Content>
+        </Layout>
+      </Content>
+    </Layout>
+  );
+};
 
-export default Settings
+export default ManagerDashboard;
